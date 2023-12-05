ac0895012defd90fea08dd5bd43b9461595178f0
Sagar Rathod <97072541+sagarbrathod1@users.noreply.github.com>
2023-12-05 15:23:20 -0500

    Initial Commit

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by Sagar Rathod on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
